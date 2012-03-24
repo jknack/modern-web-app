f7573b4dc47d366c18c5f086380d96dd802ce6cb
Edgar Espina <espina.edgar@gmail.com>
2012-03-24 14:33:27 -0300

    * Use new BindHandlerExceptionRevolver * Remove querydsl-jpa dependency

--- mwa-jpa/src/main/java/org/knowhow/mwa/jpa/JSR303HandlerExceptionResolver.java
+++ mwa-jpa/src/main/java/org/knowhow/mwa/jpa/JSR303HandlerExceptionResolver.java
@@ -1,16 +1,7 @@
 package org.knowhow.mwa.jpa;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
-import org.knowhow.mwa.handler.MessageConverterHandlerExceptionResolver;
+import org.knowhow.mwa.handler.BindHandlerExceptionResolver;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 /**
@@ -21,7 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
  * @since 0.1
  */
 public class JSR303HandlerExceptionResolver extends
-    MessageConverterHandlerExceptionResolver {
+    BindHandlerExceptionResolver {
 
   /**
    * Creates a new {@link JSR303HandlerExceptionResolver}.
@@ -31,40 +22,12 @@ public class JSR303HandlerExceptionResolver extends
   }
 
   /**
-   * Use a custom format for {@link MethodArgumentNotValidException}.
-   * <ol>
-   *  <li>field: The field's name. Optional.
-   *  <li>rejectedValue: The rejected value. Optional.
-   *  <li>message: The error message.
-   *  <li>type: The error type.
-   *  <li>source: The root/contianer object.
-   * </ol>
    * {@inheritDoc}
    */
   @Override
   protected Object convert(final Exception exception) {
-    BindingResult bindingResult =
-        ((MethodArgumentNotValidException) exception).getBindingResult();
-    List<ObjectError> bindingErrors = bindingResult.getAllErrors();
-    List<Object> errors = new ArrayList<Object>(bindingErrors.size());
-    for (ObjectError objectError : bindingErrors) {
-      String field = null;
-      Object rejectedValue = null;
-      if (objectError instanceof FieldError) {
-        FieldError fieldError = (FieldError) objectError;
-        field = fieldError.getField();
-        rejectedValue = fieldError.getRejectedValue();
-      }
-      Map<String, Object> error = new HashMap<String, Object>();
-      error.put("field", field);
-      error.put("rejectedValue", rejectedValue);
-      error.put("message", (StringUtils.trimToEmpty(field) + " " + objectError
-          .getDefaultMessage()).trim());
-      error.put("type", objectError.getCode());
-      error.put("source", objectError.getObjectName());
-      errors.add(error);
-    }
-    return errors;
+    return super.convert(((MethodArgumentNotValidException) exception)
+        .getBindingResult());
   }
 
 }
